cb591139e01d0252a7dd79758fe0100dc436fef7
aserdar05 <serdaraydin1987@gmail.com>
2018-11-27 15:46:36 +0300

    Portfolio site initial commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+    /*
+        * Open the drawer when the menu ison is clicked.
+        */
+    var menu = document.querySelector('#menu');
+    var main = document.querySelector('main');
+    var drawer = document.querySelector('.nav');
+
+    menu.addEventListener('click', function (e) {
+        drawer.classList.toggle('open');
+        e.stopPropagation();
+    });
+    main.addEventListener('click', function () {
+        drawer.classList.remove('open');
+    });
+
+});
